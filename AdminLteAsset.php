4cb0d0da68e9f86859eddde9cffb858c000d85b0
Ali Irani <irani.2007@gmail.com>
2017-04-16 01:59:02 +0430

    Fix bug in asset bundles

--- AdminLteAsset.php
+++ AdminLteAsset.php
@@ -1,48 +1,11 @@
 <?php
 namespace airani;
 
-use yii\base\Exception;
-
 /**
  * AdminLte AssetBundle
  * @author Ali Irani <ali@irani.im>
  */
-class AdminLteAsset extends \yii\web\AssetBundle
+class AdminLteAsset extends \dmstr\web\AdminLteAsset
 {
-    public $sourcePath = '@bower/adminlte/dist';
-    public $css = [
-        'css/AdminLTE.min.css',
-    ];
-    public $js = [
-        'js/app.min.js'
-    ];
-    public $depends = [
-        'rmrevin\yii\fontawesome\AssetBundle',
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
-        'yii\bootstrap\BootstrapPluginAsset',
-    ];
-
-    /**
-     * @var string|bool Choose skin color, eg. `'skin-blue'` or set `false` to disable skin loading
-     * @see https://almsaeedstudio.com/themes/AdminLTE/documentation/index.html#layout
-     */
-    public $skin = '_all-skins';
-
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        // Append skin color file if specified
-        if ($this->skin) {
-            if (('_all-skins' !== $this->skin) && (strpos($this->skin, 'skin-') !== 0)) {
-                throw new Exception('Invalid skin specified');
-            }
-
-            $this->css[] = sprintf('css/skins/%s.min.css', $this->skin);
-        }
-
-        parent::init();
-    }
+    public $css = [];
 }
--- AdminLteRtlAsset.php
+++ AdminLteRtlAsset.php
@@ -1,13 +1,11 @@
 <?php
 namespace airani;
 
-use yii\base\Exception;
-
 /**
  * AdminLteRtl AssetBundle
  * @author Ali Irani <ali@irani.im>
  */
-class AdminLteRtlAsset extends AdminLteAsset
+class AdminLteRtlAsset extends \yii\web\AssetBundle
 {
     public $sourcePath = '@bower/adminlte_rtl/dist';
 
@@ -17,27 +15,7 @@ class AdminLteRtlAsset extends AdminLteAsset
     ];
 
     public $depends = [
-        'rmrevin\yii\fontawesome\AssetBundle',
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
-        'yii\bootstrap\BootstrapPluginAsset',
+        'airani\AdminLteAsset',
         'airani\bootstrap\BootstrapRtlAsset',
     ];
-
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        // Append skin color file if specified
-        if ($this->skin) {
-            if (('_all-skins' !== $this->skin) && (strpos($this->skin, 'skin-') !== 0)) {
-                throw new Exception('Invalid skin specified');
-            }
-
-            $this->css[] = sprintf('css/skins/%s-rtl.min.css', $this->skin);
-        }
-
-        parent::init();
-    }
 }
